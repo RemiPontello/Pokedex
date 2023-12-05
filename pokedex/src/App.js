a7165c0f57f6218704ddcf0374230ee63bab49f3
RemiPontello <remi.pontello@ecoles-epsi.net>
2023-12-05 15:29:52 +0100

    c1

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import '../src/components/infobox.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-
-
       </header>
+      <infobox />
     </div>
   );
 }
---
+++ pokedex/src/components/infobox.jsx
@@ -0,0 +1,17 @@
+import './infobox.css';
+
+function infobox(props){
+
+    return(
+        <div className='box'>
+            <div className='info'>
+                    <p>info pokemon</p>
+            </div>
+
+        </div>
+    )
+ 
+}
+
+
+export default infobox
