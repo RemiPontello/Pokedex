9628e148df2b43173f10b31801a40128e1c45419
Trang <nguyenthuytrang.fr@gmail.com>
2024-01-01 21:05:44 +0100

    ....

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React,  { useState } from 'react';
 
-const PokemonDetail = ({ pokemon, onBackClick }) => {
+
+
+
+const PokemonDetail = ({ pokemon, pokemonList, onBackClick }) => {
+  const [displayShiny, setDisplayShiny] = useState(false);
+  const handleToggleShiny = () => {
+    setDisplayShiny((prevDisplayShiny) => !prevDisplayShiny);
+  };
   return (
     <div>
       <h2>{pokemon.name.en}</h2>
-      <img src={pokemon.image} alt={pokemon.name.en} style={{ width: '200px', height: '200px', marginRight: '100px' }}></img>
+      <div>
+        <label>Display:</label>
+        <button onClick={handleToggleShiny} style={{ marginLeft: '10px' }}>
+          {displayShiny ? 'Regular' : 'Shiny'}
+        </button>
+      </div>
+      <img
+        src={displayShiny ? pokemon.image_shiny : pokemon.image}
+        alt={pokemon.name.en}
+        style={{ width: '200px', height: '200px', marginRight: '100px' }}
+      />
       <p>ID: {pokemon.id}</p>
       <p>Name: {pokemon.name.en}</p>
       <p>Generation: {pokemon.generation} </p>
@@ -17,8 +34,25 @@ const PokemonDetail = ({ pokemon, onBackClick }) => {
           <li key={statName}>{`${statName}: ${statValue}`}</li>
         ))}
       </ul>
+      <p>Evolution Tree:</p>
+      <div style={{ marginLeft: '20px' }}>
+        <p>Evolved from:</p>
+        <ul>
+          {Object.entries(pokemon.evolvedFrom).map(([id, niveau]) => (
+            <li key={id}>{`${id}: ${niveau}`}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div style={{ marginLeft: '20px' }}>
+        <p>Evolved to:</p>
+        <ul>
+          {Object.entries(pokemon.evolvesTo).map(([id, niveau]) => (
+            <li key={id}>{`${id}: ${niveau}`}</li>
+          ))}
+        </ul>
+      </div>
       <button onClick={onBackClick} style={{ fontSize: '20px', padding: '10px' }} >Back</button>
-      
     </div>
   );
 };
